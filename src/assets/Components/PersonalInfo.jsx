58be581ed252c8d1b787994abf2df1c0f75fbd40
Pedro Correia <p.correia@hotmail.com>
2023-02-09 17:45:57 -0300

    Salva os dados do formulario em um estado

--- src/assets/Components/PersonalInfo.jsx
+++ src/assets/Components/PersonalInfo.jsx
@@ -1,25 +1,42 @@
-import { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function PersonalInfo(){
 
-    const [input, setInput] = useState('')
+
+    const initialValues = {
+        name: '',
+        email: '',
+        phone: ''
+    }
+
+    const handleChange = (e) => {
+        setInput({...input, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        console.log(input)
+    }
+
+    const [input, setInput] = useState(initialValues);
+
     return (
         <>
             <h1 className="pageOneTitle">Personal Info</h1>
             <p className="pageOneSubtitle">Please Provide your name, email address, and phone number.</p>
 
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <div>
                     <p>Name</p>
-                    <input className="inputForm" value={input} type="text" name="name" placeholder="e.g. John Doe" id="name" onChange={e => setInput(e.value)}/>
+                    <input className="inputForm" value={input.name} type="text" name="name" placeholder="e.g. John Doe" id="name" onChange={handleChange}/>
                 </div>
                 <div>
                     <p>Email Address</p>
-                    <input className="inputForm" type="text" name="email" placeholder="e.g john.doe@email.com" id="email" />
+                    <input className="inputForm" value={input.email} type="text" name="email" placeholder="e.g john.doe@email.com" id="email" onChange={handleChange}/>
                 </div>
                 <div>
                     <p>Phone Number</p>
-                    <input className="inputForm" type="text" name="phone" id="phone" placeholder="e.g. +1 234 567 890" required/>
+                    <input className="inputForm" value={input.phone} type="text" name="phone" id="phone" placeholder="e.g. +1 234 567 890" required onChange={handleChange}/>
                 </div>
                 <div className="button">
                     <button className="nextStep" type="submit">Next Step</button>
